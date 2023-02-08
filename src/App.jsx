4411e36fc84808b596de57fdda7815e096349151
khuslen <khuslen@nest-30s-iMac.local>
2023-02-08 19:06:21 +0800

    faf

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import zurag from '../assets/1.jpeg'
+const styles={
+  zurag:{
+    height:"100vh",
+    width:"100vw",
+  }
+}
 export const App = () => {
   return (
-    <div>asdadasdsadsa</div>
+    <img src={zurag} style={styles.zurag}/>
   )
 }
 export default App
